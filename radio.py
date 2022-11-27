8cbef9e2bfee6ff467c74bdf114fe4968e31001b
Parth Parikh <parthparikh1999p@gmail.com>
2022-11-27 02:47:50 -0500

    Removed logic for temp dir Replaced it uuid Moved the list of phones out of the code Added AM/PM during wakeup

--- radio.py
+++ radio.py
@@ -6,6 +6,7 @@ import glob
 import json
 import random
 import re
+import uuid
 import os
 from pathlib import Path
 
@@ -112,19 +113,25 @@ class Recommend:
 
 
 class Dialogue:
-    def __init__(self, file="./schema.json"):
+    def __init__(self, schema_f="./schema.json", phones_f="./data/phones.json"):
         self.rec = Recommend()
         self.synthesizer = self.init_speech()
-        with open(file, "r", encoding="UTF-8") as f:
+        with open(schema_f, "r", encoding="UTF-8") as f:
             self.schema = json.load(f)
+        with open(phones_f, "r", encoding="UTF-8") as f:
+            self.phones = json.load(f)
         self.index = 0
+        # Used to store intermediate audio clips
+        self.audio_dir = "./" + uuid.uuid4().hex[:10]
+        os.makedirs(self.audio_dir)
 
     def wakeup(self):
         now = datetime.datetime.now()
+        period = "PM" if now.hour > 12 else "AM"
         speech = (
             "You are tuning into Phoenix ten point one! "
             "I am your host Charlie. "
-            f"It is {now.hour} {now.minute} in my studio and "
+            f"It is {now.hour - 12} {now.minute} {period} in my studio and "
             "I hope that you are having a splendid day so far!"
         )
         return speech
@@ -136,10 +143,10 @@ class Dialogue:
         if self.rec.question is None:
             ques = self.rec.daily_question()
             speech = (
-                "And now it is time for today's daily question! "
+                "And now it is time for today's daily question. "
                 f"Are you Ready? Alright! Today's question is - {ques} "
                 "You can post your answers on Twitter, hashtag phoenix ten point one, "
-                "and we will read it on air. "
+                "and we will read it on our broadcast. "
             )
             return speech
         if self.rec.question:
@@ -149,7 +156,7 @@ class Dialogue:
                 "Wow! Seems like your answers to our daily question are rolling in. "
                 f"My favorite is from {fname} {lname} from {loc}. "
                 f"They say that - {ans}"
-                f" That was a fun one! Thank you {fname}! "
+                f" A great response! Thank you {fname}! "
             )
             return speech
 
@@ -157,7 +164,7 @@ class Dialogue:
         speech = (
             "And that's it for today's broadcast! "
             "Thanks for listening to Phoenix ten point one! "
-            "Hope you have a great day ahead! Bye Bye! "
+            "Hope you have a great day ahead! See You! "
         )
         return speech
 
@@ -175,7 +182,7 @@ class Dialogue:
     def weather(self, location):
         forecast = self.rec.weather(location)
         speech = (
-            f'It seems like the weather today is going to be {forecast["weather"]}. '
+            f'It seems like the weather today in {location} is going to be {forecast["weather"]}. '
             f'Presently, it is {forecast["c"]} degree celcius and {forecast["f"]} degree fahrenheit. '
             f'There seems to be a {forecast["rain"]} percent chance of rain in the next hour. '
             f'The cloud cover is {forecast["cloudcover"]} percent with a wind speed of {forecast["windspeedKmph"]} kilo-meters per hour. '
@@ -192,14 +199,14 @@ class Dialogue:
         args.SONG_NAME = [song]
         args.choice = 1
         args.quiet = True
-        ytmdl.defaults.DEFAULT.SONG_DIR = "./temp/"
+        ytmdl.defaults.DEFAULT.SONG_DIR = self.audio_dir
         ytmdl.main.main(args)
 
         # Logic to change name
         # ytmdl does not natively support this feature
-        mp3 = glob.glob(os.path.join("./temp/", "*.mp3"))[0]
+        mp3 = glob.glob(os.path.join(self.audio_dir, "*.mp3"))[0]
         sound = AudioSegment.from_mp3(mp3)
-        sound.export(f"./temp/a{self.index}.wav", format="wav")
+        sound.export(f"{self.audio_dir}/a{self.index}.wav", format="wav")
         os.remove(mp3)
         self.index += 1
 
@@ -252,11 +259,13 @@ class Dialogue:
                 case _:
                     pass
         self.radio()
+        self.cleanup()
         return 0
 
     def radio(self):
         infiles = [
-            AudioSegment.from_file(f"./temp/a{i}.wav") for i in range(self.index)
+            AudioSegment.from_file(f"{self.audio_dir}/a{i}.wav")
+            for i in range(self.index)
         ]
         outfile = "radio.wav"
         base = infiles.pop(0)
@@ -264,51 +273,30 @@ class Dialogue:
             base = base.append(infile)
         base.export(outfile, format="wav")
 
+    def cleanup(self):
+        src = "radio.wav"
+        dest = "radio.mp3"
+        # Check if dest exists and delete it
+        # as FFmpeg cannot edit existing files in-place
+        if Path(dest).is_file():
+            os.remove(dest)
+        convert = ffmpy.FFmpeg(
+            inputs={src: None},
+            outputs={dest: ["-acodec", "libmp3lame", "-b:a", "128k"]},
+        )
+        convert.run()
+
+        os.remove(src)
+        for index in range(self.index):
+            os.remove(f"{self.audio_dir}/a{index}.wav")
+
     def cleaner(self, speech):
-        abbreviations = {
-            "a": "ae",
-            "b": "bee",
-            "c": "sieh",
-            "d": "dea",
-            "e": "ee",
-            "f": "F.",
-            "g": "jie",
-            "h": "edge",
-            "i": "eye",
-            "j": "jay",
-            "k": "kaye",
-            "l": "elle",
-            "m": "emme",
-            "n": "en",
-            "o": "owe",
-            "p": "pea",
-            "q": "queue",
-            "r": "are",
-            "s": "esse",
-            "t": "tea",
-            "u": "hugh",
-            "v": "vee",
-            "w": "doub you",
-            "x": "ex",
-            "y": "why",
-            "z": "zee",
-            "1": "one",
-            "2": "two",
-            "3": "three",
-            "4": "four",
-            "5": "five",
-            "6": "six",
-            "7": "seven",
-            "8": "eight",
-            "9": "nine",
-            "0": "zero",
-        }
         acronyms = re.findall("[A-Z](?:[\\.&]?[A-Z]){1,7}[\\.]?|[A-Z][\\.]", speech)
         for acronym in acronyms:
             cleaned_up = acronym.replace(".", "")
             pronounce = str()
             for alphabet in cleaned_up:
-                pronounce += abbreviations[alphabet.lower()] + " "
+                pronounce += self.phones[alphabet.lower()] + " "
             speech = speech.replace(acronym, pronounce.replace(".", ""))
         return english_cleaners(
             speech.replace("..", ".").replace("’", "'").replace(".,", ",")
@@ -335,12 +323,12 @@ class Dialogue:
 
     def slow_it_down(self, start_index):
         for index in range(start_index, self.index):
-            src = f"./temp/a{index}.wav"
-            dest = "./temp/out.wav"
+            src = f"{self.audio_dir}/a{index}.wav"
+            dest = f"{self.audio_dir}/out.wav"
             slowit = ffmpy.FFmpeg(
                 global_options=["-y"],
                 inputs={src: None},
-                outputs={dest: ["-filter:a", "atempo=0.9"]},
+                outputs={dest: ["-filter:a", "atempo=0.85"]},
             )
             slowit.run()
             # FFmpeg cannot edit existing files in-place
@@ -350,13 +338,13 @@ class Dialogue:
     def background_music(self, file="./data/loboloco.wav"):
         background = AudioSegment.from_wav(file)
         background -= 25  # reduce the volume
-        speech = AudioSegment.from_wav(f"./temp/a{self.index - 1}.wav")
+        speech = AudioSegment.from_wav(f"{self.audio_dir}/a{self.index - 1}.wav")
         imposed = background.overlay(speech, position=4000)
-        imposed.export(f"./temp/a{self.index - 1}.wav", format="wav")
+        imposed.export(f"{self.audio_dir}/a{self.index - 1}.wav", format="wav")
 
     def silence(self):
         no_audio = AudioSegment.silent(duration=2000)
-        no_audio.export(f"./temp/a{self.index}.wav", format="wav")
+        no_audio.export(f"{self.audio_dir}/a{self.index}.wav", format="wav")
         self.index += 1
 
     def init_speech(self):
@@ -385,7 +373,7 @@ class Dialogue:
 
     def save_speech(self, text):
         wavs = self.synthesizer.tts(text, speaker_name="p267", style_wav="")
-        self.synthesizer.save_wav(wavs, f"./temp/a{self.index}.wav")
+        self.synthesizer.save_wav(wavs, f"{self.audio_dir}/a{self.index}.wav")
         self.index += 1
 
 
